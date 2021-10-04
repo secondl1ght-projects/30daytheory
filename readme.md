6077f66a4aced00c914d3d4e7e5440321f56bfef
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-10-03 21:42:27 -0600

    Pushing all work done locally

---
+++ readme.md
@@ -0,0 +1,71 @@
+# 30DAYTHEORY
+
+A travel inspiration website where you can create a list of destinations and randomly generate your next trip. You can also follow links to start researching your selected destination.
+
+Link to site: <https://www.30daytheory.world>
+
+## Table of Contents
+
+-   [General Info](#general-information)
+-   [Technologies Used](#technologies-used)
+-   [Features](#features)
+-   [Screenshots](#screenshots)
+-   [Usage](#usage)
+-   [Project Status](#project-status)
+-   [Room for Improvement](#room-for-improvement)
+-   [Acknowledgements](#acknowledgements)
+-   [Contact](#contact)
+
+## General Information
+
+This project was created as freelance work, the client wanted me to create a site based around their idea of '30daytheory'.
+
+## Technologies Used
+
+-   HTML
+-   CSS
+-   JavaScript
+
+## Features
+
+-   Randomly generate your next trip from a list of destinations chosen by you
+-   Click on the Airbnb, Tripadivsor, Wikipedia or DuckDuckGo links to begin your research
+-   Get inspired to travel and embrace somewhere!
+
+## Screenshots
+
+![screenshot](./images/screenshot.png)
+
+## Usage
+
+Enter as many potential destinations as you would like to add them to your list.
+Press the next button to generate a random trip from your list and add it to your 30daytheory.
+You can do some preliminary research on each destination by clicking on the links provided.
+Book your trip and pack your bags!
+
+## Project Status
+
+Project is: _in progress_
+
+The web app is live but I will continue to make improvements in the future.
+
+## Deployment Status
+
+Deployed
+
+## Room for Improvement
+
+-   Feedback is appreciated
+
+## Acknowledgements
+
+-   Fonts by Google Fonts
+-   Icons by (<https://www.fontawesome.com>)
+-   Coded using Atom text editor
+-   Background image Photo by <a href="https://unsplash.com/@drewmark?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andrew Stutesman</a> on <a href="https://unsplash.com/s/photos/world-map?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+## Contact
+
+If you would like to contact me please send me a message on Discord @secondl1ght#9164 or by e-mail at secondl1ght@protonmail.com, I would love to hear from you!
+
+Personal portfolio website: <https://www.secondl1ght.site>
---
+++ scripts.js
@@ -0,0 +1,43 @@
+const whereToNext = document.querySelector("#wheretonext");
+const resultsDiv = document.querySelector(".results");
+const messageDiv = document.querySelector(".message");
+const tripList = [];
+let tripSelected = [];
+const list = document.querySelector("#list");
+const links = document.querySelector(".links");
+
+function randomNumber() {
+  let number = Math.floor(Math.random() * tripList.length);
+  return number;
+}
+
+function addToList() {
+  tripList.push(document.querySelector("#destination").value);
+  document.querySelector(
+    "#count"
+  ).innerHTML = `No. of destinations: ${tripList.length}`;
+  console.log(tripList);
+}
+
+function chooseLocation() {
+  let chosenLocation = tripList[randomNumber()];
+  console.log(chosenLocation);
+  let selectedBoolean = tripSelected.includes(chosenLocation);
+  if (tripList.length === 0) {
+    messageDiv.innerHTML = "Please add destinations to your list.";
+  } else if (!selectedBoolean) {
+    messageDiv.innerHTML = "";
+    list.insertAdjacentHTML("beforeend", `<li>${chosenLocation}</li>`);
+    links.innerHTML = `<a href="https://airbnb.com/s/${chosenLocation}" target='_blank' rel='noreferrer' id='link1'><img src="./images/airbnb.png" alt="Airbnb logo" id='airbnb'><img src="./images/airbnb2.png" alt="Airbnb logo" id='airbnb2'></a><a href="https://www.tripadvisor.com/Search?q=${chosenLocation}" target='_blank' rel='noreferrer' id='link4'><img src="./images/trip.jpg" alt="Tripadvisor logo" id='trip'><img src="./images/trip2.jpg" alt="Tripadvisor logo" id='trip2'></a><a href="https://wikiless.org/w/index.php?search=${chosenLocation}&title=Special%3ASearch&fulltext=Search&ns0=1" target='_blank' rel='noreferrer' id='link2'><img src="./images/wiki.jpg" alt="Wikipedia logo" id='wiki'><img src="./images/wiki2.jpg" alt="Wikipedia logo" id='wiki2'></a><a href="https://duckduckgo.com/?q=${chosenLocation}&ia=web" target='_blank' rel='noreferrer' id='link3'><img src="./images/ddg.jpg" alt="DDG logo" id='ddg'><img src="./images/ddg2.jpg" alt="DDG logo" id='ddg2'></a>`;
+    tripSelected.push(chosenLocation);
+  } else if (tripList.length === tripSelected.length) {
+    messageDiv.innerHTML = "Your trip list is complete!";
+  } else if (selectedBoolean) {
+    chooseLocation();
+  }
+
+  console.log(tripSelected);
+}
+
+document.querySelector("#add").onclick = addToList;
+whereToNext.onclick = chooseLocation;
